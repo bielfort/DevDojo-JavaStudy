7619f918264c8555990bc3a7d012bb5ada73edcb
Gabriel <gabriel.leal.belfort@gmail.com>
2022-08-18 18:11:39 -0300

    Commit 4

---
+++ src/javacore/introducaoclasses/dominio/Carro.java
@@ -0,0 +1,9 @@
+package javacore.introducaoclasses.dominio;
+
+public class Carro {
+    public String modelo;
+    public String fabricante;
+    public int ano;
+    public String cor;
+
+}
---
+++ src/javacore/introducaoclasses/test/TestCarro01.java
@@ -0,0 +1,32 @@
+package javacore.introducaoclasses.test;
+
+import javacore.introducaoclasses.dominio.Carro;
+
+public class TestCarro01 {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+
+        carro1.modelo = "Celta";
+        carro1.fabricante = "Chevrolet";
+        carro1.cor = "Preto";
+        carro1.ano = 2010;
+
+        carro2.modelo = "Fuscão";
+        carro2.fabricante = "Volkswagen";
+        carro2.cor = "Azul";
+        carro2.ano = 1973;
+
+        System.out.println("\nCarro 1\n");
+        System.out.println(carro1.modelo);
+        System.out.println(carro1.fabricante);
+        System.out.println(carro1.ano);
+        System.out.println(carro1.cor);
+
+        System.out.println("\nCarro 2\n");
+        System.out.println(carro2.modelo);
+        System.out.println(carro2.fabricante);
+        System.out.println(carro2.ano);
+        System.out.println(carro2.cor);
+    }
+}
