ce30d2dab1db2f42023823bd982afc97592a7d56
Gabriel <gabriel.leal.belfort@gmail.com>
2022-08-23 13:21:09 -0300

    Commit 6

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -20,4 +20,20 @@ public class Calculadora {
         }
         return (num1/num2);
     }
+
+    public double divideDoisNumeros02(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        }
+        System.out.println("Não existe divisão por zero.");
+        return 0;
+
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0){
+            System.out.println("Não existe divisão por zero");
+        }
+        System.out.println(num1/num2);
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -5,7 +5,10 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculad
 public class CalculadoraTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        double result = (int) calculadora.divideDoisNumeros(20, 2);
+        double result = (int) calculadora.divideDoisNumeros(20, 0);
         System.out.println(result);
+        System.out.println(calculadora.divideDoisNumeros02(20,0));
+        System.out.println("-----------------");
+        calculadora.imprimeDivisaoDeDoisNumeros(86,2);
     }
 }
