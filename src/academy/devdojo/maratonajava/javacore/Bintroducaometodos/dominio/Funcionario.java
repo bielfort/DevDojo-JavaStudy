27306e71e907841e59be26e6779537dc466ba9c9
Gabriel <gabriel.leal.belfort@gmail.com>
2022-08-29 15:14:22 -0300

    Commit 14

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,29 +1,61 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-        public String nome;
-        public int idade;
-        public double[] salario;
+        private String nome;
+        private int idade;
+        private double[] salario;
+        private double media;
 
         public void imprime(){
             System.out.println(this.nome);
             System.out.println(this.idade);
-            for (double salario: salario){
-                System.out.println(salario + " ");
-            }
-        }
-        public void imprimeMediaSalario(){
             if (salario == null){
                 return;
             }
-            double media = 0;
-            for (double salario: salario){
+            for (double salario : salario){
+                System.out.println(salario + " ");
+            }
+        }
+        public void imprimeMediaSalario() {
+            if (salario == null){
+                return;
+            }
+            for (double salario : salario){
                 media += salario;
             }
             media /= salario.length;
-            System.out.println("Média salarial: " + media);
+            System.out.println("/nMédia salarial" + media);
         }
+
+    public String getNome() {
+        return nome;
     }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double[] salario) {
+        this.salario = salario;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+}
 
 
 
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/MetodoFuncionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/MetodoFuncionario.java
@@ -5,12 +5,11 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Funciona
 public class MetodoFuncionario {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Gabriel";
-        funcionario.idade = 27;
-        funcionario.salario = new double[]{1900, 1500, 1800};
-
+        funcionario.setNome("Gabriel");
+        funcionario.setIdade(27);
+        funcionario.setSalario(new double[]{1900, 1500, 1800});
         funcionario.imprime();
-        funcionario.imprimeMediaSalario();
 
+        System.out.println(funcionario.getMedia());
     }
 }
