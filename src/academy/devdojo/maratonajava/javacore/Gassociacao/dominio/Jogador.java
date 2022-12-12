5db665f456cd9f43dde80917d8af69c94220f120
Gabriel <gabriel.leal.belfort@gmail.com>
2022-12-12 15:12:11 -0300

    Commit 22

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Jogador.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Jogador.java
@@ -2,9 +2,21 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 
 public class Jogador {
     private String nome;
+    private Time time;
 
     public void imprime() {
         System.out.println(this.nome);
+        if (time != null){
+            System.out.println(time.getNome());
+        }
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
     }
 
     public Jogador(String nome) {
@@ -18,4 +30,5 @@ public class Jogador {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
 }
